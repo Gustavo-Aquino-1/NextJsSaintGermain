8a185a250b6315e648ff1ae3f3ee3102149fa9ef
Gustavo-Aquino-1 <guhaquino10@gmail.com>
2024-03-07 15:52:34 -0300

    feat: meal page

--- app/meals/[id]/page.js
+++ app/meals/[id]/page.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import classes from './page.module.css'
+import Image from 'next/image'
+import { getMeal } from '@/lib/meals'
 
-function Meal({ params: { id } }) {
+function Meal({ params: { id: slug } }) {
+  const meal = getMeal(slug)
+  console.log(meal)
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />')
   return (
-    <div>
-      <p>Page of the Meal {id}</p>
-    </div>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+            {/* go to your app to send email */}
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main className={classes.main}>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   )
 }
 
---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <main className='not-found'>
+      <h1>Not Found</h1>
+      <p>This page or resource was not found</p>
+    </main>
+  )
+}
+
+export default NotFound
